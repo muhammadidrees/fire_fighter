447f4dd985b2221596e0e448ae2e0448209262c5
Muhammad Idrees <midrees097@gmail.com>
2023-12-09 15:43:34 +0500

    managed to move the firehose to bottom center

--- lib/components/fire_hose.dart
+++ lib/components/fire_hose.dart
@@ -3,6 +3,12 @@ part of 'components.dart';
 class FireHose extends PositionComponent {
   static final _paint = Paint()..color = Colors.white;
 
+  FireHose(Vector2 canvasSize) {
+    position = Vector2(canvasSize.x / 2, canvasSize.y - 80);
+    size = Vector2(50, 50);
+    anchor = Anchor.center;
+  }
+
   @override
   void render(Canvas canvas) {
     canvas.drawRect(size.toRect(), _paint);
--- lib/game/fire_fighter_game.dart
+++ lib/game/fire_fighter_game.dart
@@ -3,11 +3,7 @@ part of 'game.dart';
 class FireFighterGame extends FlameGame {
   @override
   Future<void> onLoad() async {
-    final fireHose = FireHose()
-      ..position = size / 2
-      ..width = 50
-      ..height = 100
-      ..anchor = Anchor.center;
+    final fireHose = FireHose(size);
 
     add(fireHose);
   }
