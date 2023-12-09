58952c551593d5bb9e4ef347b33fd5f4a2367605
Muhammad Idrees <midrees097@gmail.com>
2023-12-09 15:15:01 +0500

    first commit

---
+++ android/app/src/main/kotlin/org/flame_engine/games/fire_fighter/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.flame_engine.games.fire_fighter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/components.dart
@@ -0,0 +1,1 @@
+part 'fire_hose.dart';
