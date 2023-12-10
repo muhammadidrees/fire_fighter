4bd5e1c9d9bedbe36e13ec14a0257a2dd68f6d28
Muhammad Idrees <midrees097@gmail.com>
2023-12-10 18:40:15 +0500

    added credits

--- lib/main.dart
+++ lib/main.dart
@@ -106,10 +106,32 @@ class GameOverOverlay extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(
-            'Game Over',
+            'Game Over!',
             style: TextStyle(
               color: Colors.white,
               fontSize: 32,
+              fontFamily: 'PressStart2P',
+            ),
+          ),
+          SizedBox(height: 24),
+          Text(
+            'Thank you for playing the game',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontFamily: 'PressStart2P',
+            ),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'This game was created using Flutter and Flame\n\n'
+            'for the contest Flame Jam 3.0.\n\n\n'
+            'You can get the source code from:\n\nhttps://www.github.com/muhammadidrees/fire_fighter',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12,
               fontFamily: 'PressStart2P',
             ),
           ),
