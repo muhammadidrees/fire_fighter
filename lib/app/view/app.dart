369330c665b45031409405026408bb5bf08b2f5a
Muhammad Idrees <midrees097@gmail.com>
2023-12-22 23:29:18 +0500

    added root app widget

---
+++ lib/app/view/app.dart
@@ -0,0 +1,50 @@
+import 'package:fire_fighter/loading/loading.dart';
+import 'package:flame/cache.dart';
+import 'package:flame_audio/flame_audio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => LoadingCubit(
+            Images(prefix: ''),
+            AudioCache(prefix: ''),
+          )..loadSequentially(),
+        ),
+      ],
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF2A48DF),
+        appBarTheme: const AppBarTheme(color: Color(0xFF2A48DF)),
+        colorScheme: ColorScheme.fromSwatch(
+          accentColor: const Color(0xFF2A48DF),
+        ),
+        scaffoldBackgroundColor: const Color(0xFFFFFFFF),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(const Color(0xFF2A48DF)),
+          ),
+        ),
+        fontFamily: 'PressStart2P',
+      ),
+      home: const LoadingPage(),
+    );
+  }
+}
